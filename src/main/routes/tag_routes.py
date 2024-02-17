0081eb7c2737aacb660ecbeec39b06935983fe14
Eduardo Martins <eduardo.amaral382@gmail.com>
2024-02-16 21:01:50 -0300

    feat: implementing view for tag creator with http types

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, request, jsonify
+from src.views.http_types.http_request import HttpRequest
 
 tags_routes_bp = Blueprint('tags_routes', __name__)
 
 @tags_routes_bp.route('/create_tag', methods=["POST"])
 def create_tags():
     print(request.json)
+    http_request = HttpRequest(body=request.json)
     return jsonify({ "resp": "ok"}), 200
