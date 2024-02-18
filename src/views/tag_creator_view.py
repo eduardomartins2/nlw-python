05f8a2e34fe991f4ecf5d199b720c7d58601da5b
Eduardo Martins <eduardo.amaral382@gmail.com>
2024-02-18 20:24:05 -0300

    feat: implementing controllers and drivers for function

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class tagCreatorView:
     '''
@@ -7,10 +8,15 @@ class tagCreatorView:
     '''
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreatorController()
+
         body = http_request.body
         product_code = body["product_code"]
 
         # logica de regra de negocio
+        formatted_response = tag_creator_controller.create(product_code)
+
+
 
         # retorno http
         return HttpResponse(status_code=200, body={"hello": "world"})
